ef3687fb71aad6f7a602065fbb9d96284ef8d4c3
Ainesh Nanda <67788476+Ainesh06102004@users.noreply.github.com>
2021-01-03 17:55:42 +0530

    Add files via upload

--- screens/bookdonate.js
+++ screens/bookdonate.js
@@ -2,12 +2,38 @@ import * as React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, TextInput, Alert, Modal, ScrollView, KeyboardAvoidingView } from 'react-native';
 import db from '../config';
 import firebase from 'firebase';
-import {Header} from 'react-native-elements';
+import { Header } from 'react-native-elements';
 
 
 
-export default class Bookdonate extends React.Component{
-    render(){
-        return(<View style = {{marginTop: 250, flex: 1}}><Text>Book Donate Screen</Text></View>)
+export default class Bookdonate extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            currentuser: firebase.auth().currentUser.email,
+            booklist: [],
+        }
+    }
+    getbooklist = () => {
+        var list = [];
+        db.collection('Requests').onSnapshot((snapshot) => {
+            snapshot.docs.map((doc) => {
+                var data = doc.data();
+                list.push(data);
+            });
+        });
+        console.log(list);
+        this.setState({ booklist: list })
+    }
+
+    componentDidMount() {
+        this.getbooklist();
+    }
+
+    render() {
+        return (!this.state.booklist 
+            ? <Text>No donations to be made!!</Text>
+             : (<View style={{ marginTop: 250, flex: 1 }}><Text>Hello</Text>
+        </View>))
     }
 }
--- screens/bookrequest.js
+++ screens/bookrequest.js
@@ -2,29 +2,89 @@ import * as React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, TextInput, Alert, Modal, ScrollView, KeyboardAvoidingView } from 'react-native';
 import db from '../config';
 import firebase from 'firebase';
-import { Header ,Icon} from 'react-native-elements';
+import { Header, Icon } from 'react-native-elements';
 
 
 export default class Bookrequest extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            currentuser: firebase.auth().currentUser.email,
+            bookname: '',
+            reason: ''
+        }
+    }
+    createuniqueId(){
+        return Math.random().toString(32).substring(7);
+    }
+
+    addRequest = (name, reason)=>{
+        var id = this.createuniqueId();
+        db.collection('Requests').add({
+            user: this.state.currentuser,
+            bookname: name,
+            reason: reason,
+            requestId: id
+        });
+        this.setState({
+            bookname: '',
+            reason: ''
+        })
+        Alert.alert("Book request submitted!!");
+    }
     render() {
         return (
-            <View style = {{flex: 1}}>
+            <View style={{ flex: 1 }}>
                 <Header
-                    leftComponent={<Icon name= "bars" type="font-awesome" onPress = {()=>{
+                    leftComponent={<Icon name="bars" type="font-awesome" onPress={() => {
                         this.props.navigation.toggleDrawer()
-                    }}/>}
+                    }} />}
                     rightComponent={{}}
                     centerComponent={{ text: 'Request Books!', style: { color: 'white' } }}
                 />
                 <TextInput
-                    placeholder='Name'>
-                </TextInput>
-                <TextInput
-                    placeholder='Why do you want this book?'>
-                </TextInput>
+                    placeholder='Name'
+                    onChangeText={(text) => {
+                        this.setState({
+                            bookname: text
+                        })
+                    }}
+                    value={this.state.bookname}
+                    style={{
+                        width: "75%",
+                        height: 35,
+                        alignSelf: 'center',
+                        borderColor: '#ffab91',
+                        borderRadius: 10,
+                        borderWidth: 1,
+                        marginTop: 220,
+                        padding: 10
+                    }}
+                />
 
-                <TouchableOpacity>
-                    <Text> Request </Text>
+                <TextInput
+                    placeholder='Why do you want this book?'
+                    onChangeText={(text) => {
+                        this.setState({
+                            reason: text
+                        })
+                    }}
+                    value={this.state.reason}
+                    multiline={true}
+                    style={{
+                        width: "75%",
+                        height: 35,
+                        alignSelf: 'center',
+                        borderColor: '#ffab91',
+                        borderRadius: 10,
+                        borderWidth: 1,
+                        marginTop: 20,
+                        padding: 10
+                    }} />
+                <TouchableOpacity 
+                style={{alignSelf: 'center', backgroundcolor: 'black'}}
+                onPress = {()=>{this.addRequest(this.state.bookname, this.state.reason)}}>
+                    <Text style ={{alignSelf: "center"}}>Request</Text>
                 </TouchableOpacity>
 
             </View>
