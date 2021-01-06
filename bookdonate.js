200084f507e3cceb19c3d02b8bcbe97240cb76ab
Ainesh Nanda <67788476+Ainesh06102004@users.noreply.github.com>
2021-01-06 17:58:20 +0530

    Add files via upload

---
+++ bookdonate.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, Alert, Modal, ScrollView, KeyboardAvoidingView, FlatList } from 'react-native';
+import db from '../config';
+import firebase from 'firebase';
+import { Header, ListItem } from 'react-native-elements';
+
+
+
+export default class Bookdonate extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            currentuser: firebase.auth().currentUser.email,
+            booklist: [],
+        }
+    }
+    getbooklist = () => {
+        var list = [];
+        db.collection('Requests').onSnapshot((snapshot) => {
+            var requestedBooksList = snapshot.docs.map((doc) => doc.data())
+            this.setState({
+                booklist: requestedBooksList
+            });
+        });
+
+    }
+
+    componentDidMount() {
+        this.getbooklist();
+    }
+    keyExtractor = (item, index) => index.toString()
+
+    renderItem = ({ item, i }) => {
+        return (
+            <ListItem 
+            style={{marginTop: 10}}
+            key={i} bottomDivider >
+                <ListItem.Content>
+                    <ListItem.Title>{item.bookname}</ListItem.Title>
+                    <ListItem.Subtitle>{item.reason}</ListItem.Subtitle>
+                    
+                </ListItem.Content>
+            </ListItem>
+
+        );
+    }
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                {
+                    this.state.booklist.length === 0
+                        ? (<View><Text>No donations to be made!!</Text></View>)
+                        : (
+                            <View>
+                                <FlatList
+                                    keyExtractor={this.keyExtractor}
+                                    data={this.state.booklist}
+                                    renderItem={this.renderItem} />
+                            </View>
+                        )
+                }
+            </View>
+        )
+    }
+}
