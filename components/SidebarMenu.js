1a558ef6394821afec919a0018f4468604c4740e
Ainesh Nanda <67788476+Ainesh06102004@users.noreply.github.com>
2021-01-15 17:58:31 +0530

    Add files via upload

--- components/SidebarMenu.js
+++ components/SidebarMenu.js
@@ -1,13 +1,53 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, TextInput, Alert, Modal, ScrollView, KeyboardAvoidingView } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, Alert, Modal, ScrollView, KeyboardAvoidingView, SafeAreaView } from 'react-native';
+import firebase from 'firebase';
+import { DrawerItems } from 'react-navigation-drawer'
+import { Avatar } from 'react-native-elements';
+
 
 export default class Sidebarmenu extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            image: '#'
+        }
+    }
     render() {
-        return (<View style={{
-            flex: 1,
-            justifyContent: 'flex-end',
-            paddingBottom: 30
-        }}><TouchableOpacity><Text>Log Out</Text></TouchableOpacity></View >);
+        return (<View style={{ flex: 1 }}>
+             <SafeAreaView>
+            <View
+                style={{
+                    
+                    alignItems: "center",
+                    backgroundColor: "orange",
+                    height: 60, 
+                    width: 60,
+                    marginTop: 100
+                }}
+            >
+                <Avatar rounded size = "small"/>
+            </View>
+           
+                <View style={{ marginTop: 20 }}><DrawerItems {...this.props} /></View>
+                <View style={{
+                    flex: 1,
+                    justifyContent: 'flex-end',
+                    paddingBottom: 30,
+                    marginTop: 450
+                }}><TouchableOpacity style={{
+                    height: 50,
+                    width: "100%",
+                    justifyContent: "center",
+                    padding: 10,
+                    alignSelf: 'center',
+                    backgroundColor: 'black'
+                }}
+                    onPress={() => {
+                        this.props.navigation.navigate("Loginscreen");
+                        firebase.auth().signOut();
+                    }}><Text style={{ alignSelf: 'center', color: 'white' }}>Log Out</Text></TouchableOpacity></View >
+            </SafeAreaView>
+        </View>);
     }
 
 }
